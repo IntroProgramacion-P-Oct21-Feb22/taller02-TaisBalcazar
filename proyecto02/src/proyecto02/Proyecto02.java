4f3c64eef2345e61f6663947d21e4c82c7a2a00f
John Doe <johndoe@example.com>
2021-10-22 11:58:37 -0500

    primer commit

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto02;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ejemplo dos básico de Java");
+        System.out.println("Versionando a través de Git");
+        System.out.println("Manejado desde Github");
+        System.out.println("Además de usar GithubClassroom");
+        
+
+
+
+
+        // TODO code application logic here
+    }
+    
+}
